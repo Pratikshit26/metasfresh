beebead6089fb32a2606558b8fd7c925a5885637
Pratikshit26 <pratikshitpfd@gmail.com>
2026-02-24 11:15:35 +0530

    changes to staging

--- backend/de.metas.adempiere.adempiere/base/src/test/java/org/adempiere/test/TestClientUI.java
+++ backend/de.metas.adempiere.adempiere/base/src/test/java/org/adempiere/test/TestClientUI.java
@@ -1,26 +1,6 @@
 package org.adempiere.test;
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.base
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+ 
 
 
 import java.lang.reflect.Method;
@@ -69,7 +49,7 @@ public class TestClientUI extends AbstractClientUI implements IClientUIInstance
 	private static final IAskCallback RESPONSE_No = new YesNoAskCallback(false);
 
 	private IAskCallback askCallback;
-	// private Boolean yesNoAnswer = null;
+	// Private Boolean yesNoAnswer = null;
 
 	private Method lastInvocation;
 
@@ -137,7 +117,7 @@ public class TestClientUI extends AbstractClientUI implements IClientUIInstance
 	@Override
 	public boolean ask(final int WindowNo, final String AD_Message, final String message)
 	{
-		// ADAssert.assertMsgExists(AD_Message);
+	// ADAssert.assertMsgExists(AD_Message);
 
 		final Boolean answer = getYesNoAnswer(WindowNo, AD_Message, message);
 		Assert.assertNotNull("No default answer was specified for windowNo=" + WindowNo + ", AD_Message=" + AD_Message + ", message=" + message, answer);
